cf444efb8791eb505595c6b9e63c2decce8fc803
Tian-Nian <chentianxing2002@gmail.com>
2026-03-10 22:41:50 +0800

    support instruction

--- demo_policy/deploy.py
+++ demo_policy/deploy.py
@@ -1,9 +1,10 @@
 def eval_one_episode(TASK_ENV, model_client):
-    instruction = TASK_ENV.get_instruction()
-    model_client.call(func_name="set_language", obs=instruction)
-
     while not TASK_ENV.is_episode_end(): # Check whether the episode ends
         obs = TASK_ENV.get_obs() # Get Observation
+
+        instruction = obs['instruction']
+        model_client.call(func_name="set_language", obs=instruction)
+
         model_client.call(func_name="update_obs", obs=obs)  # Update Observation, `update_obs` here can be modified
         actions = model_client.call(func_name="get_action") # Get Action according to observation chunk
         
