4aa7a7e5bed87a98e77da743a412739395b8ec9c
Tian-Nian <1250576969@qq.com>
2026-05-13 11:10:37 +0800

    remove abs route, change to /path/to/* .

--- policy/X_VLA/xvla/train.sh
+++ policy/X_VLA/xvla/train.sh
@@ -3,13 +3,13 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 accelerate launch \
     --mixed_precision bf16 \
     train.py \
-    --models '/vepfs-cnbje63de6fae220/xspark_shared/model_weights/X-VLA-Pt' \
-    --train_metas_path /vepfs-cnbje63de6fae220/niantian/RoboDojo_env/XPolicyLab/policy/X-VLA/xvla/meta.json \
+    --models '/path/to/X-VLA-Pt' \
+    --train_metas_path /path/to/meta.json \
     --learning_rate 1e-4 \
     --learning_coef 0.1 \
     --iters 30000 \
     --freeze_steps 1000 \
     --warmup_steps 2000 \
     --batch_size 32 \
-    --output_dir /vepfs-cnbje63de6fae220/niantian/RoboDojo_env/XPolicyLab/policy/X-VLA/checkpoints/sim_stack_bowls/ \
+    --output_dir /path/to/output_dir/ \
     --save_interval 1000 \
