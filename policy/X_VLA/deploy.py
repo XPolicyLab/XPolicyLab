fbb6acc40e403ab5d0608cab09d3ebc541f1d787
TianxingChen <chentianxing2002@gmail.com>
2026-05-17 13:59:38 +0000

    update deploy.py: add episode_end_check

--- policy/X_VLA/deploy.py
+++ policy/X_VLA/deploy.py
@@ -34,9 +34,12 @@ def eval_one_episode(TASK_ENV, model_client):
 
         for action_idx, action in enumerate(actions):
             TASK_ENV.take_action(action)
-            if action_idx != len(actions) - 1:
-                obs = _get_valid_obs(TASK_ENV)
-                model_client.call(func_name="update_obs", obs=obs)
+            
+            if TASK_ENV.is_episode_end() or action_idx + 1 == len(actions):
+                break
+            
+            obs = _get_valid_obs(TASK_ENV)
+            model_client.call(func_name="update_obs", obs=obs)
 
 
 def eval_one_episode_batch(TASK_ENV, model_client):
@@ -51,7 +54,9 @@ def eval_one_episode_batch(TASK_ENV, model_client):
             current_action_list = [env_actions[action_idx] for env_actions in actions]
             TASK_ENV.take_action_batch(current_action_list, env_idx_list)
 
-            if action_idx != len(actions) - 1:
-                env_idx_list = TASK_ENV.get_running_env_idx_list()
-                obs_list = TASK_ENV.get_obs_batch(env_idx_list)
-                model_client.call(func_name="update_obs_batch", obs=obs_list)
+            if TASK_ENV.is_episode_end() or action_idx + 1 == len(actions):
+                break
+            
+            env_idx_list = TASK_ENV.get_running_env_idx_list()
+            obs_list = TASK_ENV.get_obs_batch(env_idx_list)
+            model_client.call(func_name="update_obs_batch", obs=obs_list)
--- policy/demo_policy/deploy.py
+++ policy/demo_policy/deploy.py
@@ -7,9 +7,11 @@ def eval_one_episode(TASK_ENV, model_client):
         for action_idx, action in enumerate(actions):
             TASK_ENV.take_action(action)
             
-            if action_idx != len(actions) - 1:
-                obs = TASK_ENV.get_obs() # Get Observation
-                model_client.call(func_name="update_obs", obs=obs)
+            if TASK_ENV.is_episode_end() or action_idx + 1 == len(actions):
+                break
+            
+            obs = TASK_ENV.get_obs() # Get Observation
+            model_client.call(func_name="update_obs", obs=obs)
 
 def eval_one_episode_batch(TASK_ENV, model_client):
 
@@ -25,7 +27,9 @@ def eval_one_episode_batch(TASK_ENV, model_client):
 
             TASK_ENV.take_action_batch(current_action_list, env_idx_list)
             
-            if action_idx != len(actions) - 1:
-                env_idx_list = TASK_ENV.get_running_env_idx_list()
-                obs_list = TASK_ENV.get_obs_batch(env_idx_list) # Get Batch Observation
-                model_client.call(func_name="update_obs_batch", obs=obs_list)
+            if TASK_ENV.is_episode_end() or action_idx + 1 == len(actions):
+                break
+
+            env_idx_list = TASK_ENV.get_running_env_idx_list()
+            obs_list = TASK_ENV.get_obs_batch(env_idx_list) # Get Batch Observation
+            model_client.call(func_name="update_obs_batch", obs=obs_list)
