0add2e019f99c5991bd8c62f26d83ede24e63f77
FlowsMple <718127833@qq.com>
2026-05-23 09:09:11 +0000

    update README and demo_policy

--- policy/demo_policy/train.sh
+++ policy/demo_policy/train.sh
@@ -2,10 +2,11 @@
 set -e
 
 # ==================== 参数定义 ====================
-dataset_name=${1}
-task_name=${2}
-env_cfg_type=${3}
-expert_data_num=${4}
-action_type=${5}
-gpu_id=${6}
-seed=${7}
+dataset_name=$1 # requried
+task_name=$2
+ckpt_name=$3 # requried
+env_cfg_type=$4 # requried
+expert_data_num=$5
+action_type=$6 # requried
+seed=$7 # requried
+gpu_id=$8
