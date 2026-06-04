186fd170560ec6807f99b51b998735fb4cab5fc8
b1ngsha <cheyne94@outlook.com>
2026-06-04 15:49:29 +0800

    feat: integrate RoboDojoModelClient for WebSocket communication and enhance debug environment client with new protocol options

--- utils/setup_env_client.sh
+++ utils/setup_env_client.sh
@@ -15,16 +15,20 @@ seed="${11}"
 env_gpu_id="${12}"
 policy_server_ip="${13:-localhost}"
 
-read eval_batch eval_env < <(python - <<PY
+read eval_batch eval_env protocol < <(python - <<PY
 import yaml
 with open("${yaml_file}", "r") as f:
     data = yaml.safe_load(f)
-print(str(data.get("eval_batch", False)).lower(), data.get("eval_env"))
+print(
+    str(data.get("eval_batch", False)).lower(),
+    data.get("eval_env"),
+    data.get("protocol", "legacy_tcp"),
+)
 PY
 )
 
 if [[ "${eval_env}" == "debug" ]]; then
-    bash "${UTILS_DIR}/run_debug_env_client.sh" "${eval_batch}" "${eval_env_conda_env}" "${policy_server_port}" "${dataset_name}" "${task_name}" "${env_cfg_type}" "${policy_name}" "${additional_info}" "${ROOT_DIR}" "${seed}" "${env_gpu_id}" "${policy_server_ip}"
+    bash "${UTILS_DIR}/run_debug_env_client.sh" "${eval_batch}" "${eval_env_conda_env}" "${policy_server_port}" "${dataset_name}" "${task_name}" "${env_cfg_type}" "${policy_name}" "${additional_info}" "${ROOT_DIR}" "${seed}" "${env_gpu_id}" "${policy_server_ip}" "${protocol}"
 elif [[ "${eval_env}" == "sim" ]]; then
     bash "${UTILS_DIR}/run_sim_env_client.sh" "${eval_batch}" "${eval_env_conda_env}" "${policy_server_port}" "${dataset_name}" "${task_name}" "${env_cfg_type}" "${policy_name}" "${additional_info}" "${ROOT_DIR}" "${seed}" "${env_gpu_id}" "${policy_server_ip}"
 elif [[ "${eval_env}" == "real" ]]; then
